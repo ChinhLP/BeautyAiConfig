a3308b738324a0bbb5ed2f87e5f0d75346f3775d
le phuc chinh <ChinhLP@apero.vn>
2025-06-10 00:34:25 +0700

    refactor: remove unused initialization code in App class

--- app/src/main/java/com/apero/testai/App.kt
+++ app/src/main/java/com/apero/testai/App.kt
@@ -1,12 +1,6 @@
 package com.apero.testai
 
 import android.app.Application
-import com.apero.testai.pref.SharedPref
-import com.apero.testai.utils.beauty_full.CommonConfig
-import com.apero.testai.utils.beauty_full.BeautyFullServiceConfig
-import com.apero.testai.utils.beauty_full.VslBeautyFullAdsConfig
-import com.apero.testai.utils.beauty_full.BeautyConfigProvider
-import com.apero.beauty_full.VslBeautyFullEntry
 
 class App : Application() {
 
@@ -19,18 +13,5 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         instance = this
-        
-        val commonConfig = CommonConfig(SharedPref(this), this)
-        val serviceConfig = BeautyFullServiceConfig()
-        val adsConfig = VslBeautyFullAdsConfig()
-        val beautyProvider = BeautyConfigProvider(commonConfig, adsConfig, serviceConfig)
-
-        VslBeautyFullEntry.initialize(
-            context = this,
-            commonConfig = commonConfig,
-            serviceConfig = serviceConfig,
-            allFeatureAdsIdConfig = adsConfig,
-            beautyConfigProvider = beautyProvider
-        )
     }
 }
